a971c25b34fb1dcc45fd417dffc66397936bd509
MICROPROINDIA\spatil <spatil@microproindia.com>
2025-04-28 21:02:11 +0530

    model changes

--- src/main/java/com/complaintandfeedback/securityConfig/SecurityConfig.java
+++ src/main/java/com/complaintandfeedback/securityConfig/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/send-otp", "/api/auth/verify-otp","/api/auth/forget-password","/api/departments/getAllDepartment","/api/roles/getAllRole").permitAll()
+                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/send-otp", "/api/auth/verify-otp","/api/auth/forget-password","/api/departments/getAllDepartment"
+                		,"/api/roles/getAllRole","/api/organdopr/getAllOrgs","/api/organdopr/getAllOprs").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )

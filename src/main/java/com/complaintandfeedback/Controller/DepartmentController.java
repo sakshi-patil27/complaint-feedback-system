c0a70bee1bd789b518a8a508d82c4938454cb168
HussainD-2203 <hussaindrbr@gmail.com>
2025-04-29 14:23:32 +0530

    changes

--- src/main/java/com/complaintandfeedback/Controller/DepartmentController.java
+++ src/main/java/com/complaintandfeedback/Controller/DepartmentController.java
@@ -1,16 +1,16 @@
 package com.complaintandfeedback.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.complaintandfeedback.DTO.CommonRequestModel;
 import com.complaintandfeedback.Model.Department;
 import com.complaintandfeedback.Service.DepartmentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/departments")

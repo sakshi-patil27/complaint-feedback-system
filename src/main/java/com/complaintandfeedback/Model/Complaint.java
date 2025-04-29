733a15ba579b06e47ee9dc396b861ef6f7e40451
HussainD-2203 <hussaindrbr@gmail.com>
2025-04-29 14:15:29 +0530

    added Complaint model

---
+++ src/main/java/com/complaintandfeedback/Model/Complaint.java
@@ -0,0 +1,56 @@
+package com.complaintandfeedback.Model;
+
+import java.security.Timestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Complaint {
+	private String complaint_id;
+
+    private Long org_id;
+
+    private Long opr_id;
+    
+    @NotBlank(message = "Subject field cannot be empty")	
+    @Size(max = 100, min = 1, message = "Subject field should be between 1 to 100 characters")
+    private String subject;
+    
+    @NotBlank(message = "Subject field cannot be empty")	
+    @Size(max = 200, min = 1, message = "Subject field should be between 1 to 200 characters")
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String priority;
+
+    private String status;
+
+    private String department_id;
+
+    private String created_by;
+
+    private String assigned_to;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Timestamp created_on;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Timestamp modified_on;
+
+    private String modified_by;
+
+    private String attachment_id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private Timestamp due_date;
+
+    private String is_active;
+}

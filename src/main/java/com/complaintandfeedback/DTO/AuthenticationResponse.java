7b3ba7954e4072d6ff80b594448c0a15965af927
MICROPROINDIA\spatil <spatil@microproindia.com>
2025-04-29 10:15:48 +0530

    Reapply "model changes"
    This reverts commit 74ab5a928acc6dfcaa01755fc30e4a29697c361b.

--- src/main/java/com/complaintandfeedback/DTO/AuthenticationResponse.java
+++ src/main/java/com/complaintandfeedback/DTO/AuthenticationResponse.java
@@ -6,7 +6,6 @@ import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class AuthenticationResponse {
     private boolean success;
     private String message;
@@ -14,5 +13,19 @@ public class AuthenticationResponse {
     private String userId;
     private String role;
     private String username;
+	private Long org_id;	
+	private Long opr_id;
+	public AuthenticationResponse(boolean success, String message, String token, String userId, String role,
+			String username, Long org_id, Long opr_id) {
+		super();
+		this.success = success;
+		this.message = message;
+		this.token = token;
+		this.userId = userId;
+		this.role = role;
+		this.username = username;
+		this.org_id = org_id;
+		this.opr_id = opr_id;
+	}	
 }
 

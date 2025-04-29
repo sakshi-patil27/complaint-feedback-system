c9f5ca424d2244bce919f58dd5c1c8d2b1b2dcbc
HussainD-2203 <hussaindrbr@gmail.com>
2025-04-29 14:22:37 +0530

    controller

---
+++ src/main/java/com/complaintandfeedback/Controller/ComplaintController.java
@@ -0,0 +1,10 @@
+package com.complaintandfeedback.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/complaint")
+public class ComplaintController {
+
+}
---
+++ src/main/java/com/complaintandfeedback/Service/ComplaintService.java
@@ -0,0 +1,8 @@
+package com.complaintandfeedback.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComplaintService {
+
+}
